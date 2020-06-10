be02c1b65e252b9e92d3540dd0c8e0cc818f3a08
Kodjo <kodjo.klouvi@gmail.com>
2020-06-10 20:05:21 +0200

    implement airflow pipeline

---
+++ dags/model.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+import pickle
+
+
+# This filepath should be passed as an argument
+def train_model(dataset_filepath, trained_model_path):
+    dataset = pd.read_csv(dataset_filepath)
+
+    attributes = dataset.iloc[:, 0:4].values
+    labels = dataset.iloc[:, 4].values
+
+    # Divide the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        attributes, labels, test_size=0.2, random_state=0
+    )
+
+    # Feature Scaling
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+
+    # Train the model
+    regressor = RandomForestRegressor(n_estimators=200, random_state=0)
+    regressor.fit(X_train, y_train)
+    y_pred = regressor.predict(X_test)
+
+    # Evaluate the performance of the model
+    print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+    print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+    print('Root Mean Squared Error:', np.sqrt(
+        metrics.mean_squared_error(y_test, y_pred)))
+
+    # Save the model to disk
+
+    pickle.dump(regressor, open(trained_model_path, 'wb'))
+    print("Model saved.")
---
+++ dags/mypipeline.py
@@ -0,0 +1,91 @@
+import time
+import datetime
+from tempfile import NamedTemporaryFile
+
+import timedelta
+from builtins import range
+from model import train_model
+
+import airflow.hooks.S3_hook
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
+data_s3_path = ' https://iasd-data-in-the-cloud.s3.eu-west-3.amazonaws.com/petrol_consumption.csv'
+data_local_path = './petrol_consumption.csv'
+trained_model_local_path = './model.pickle'
+
+
+args = {
+    'owner': 'KodjoKlouvi',
+    'start_date': datetime(2020, 6, 10),
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    dag_id='ml_pipeline_example',
+    default_args=args,
+    schedule_interval=None,
+    tags=['ml', 'pipeline', 'example']
+)
+
+
+# [START ml_pipeline]
+def download_from_s3_task(key, bucket_name, output_path):
+    hook = airflow.hooks.S3_hook.S3Hook('aws_default')
+    source_object = hook.get_key(key, bucket_name)
+    source_object.get_contents_to_file(output_path)
+
+
+def upload_to_s3_task(filename, key, bucket_name):
+    hook = airflow.hooks.S3_hook.S3Hook('aws_default')
+    hook.load_file(filename, key, bucket_name)
+
+
+def echo(str):
+    print(str)
+
+
+start_task = PythonOperator(
+    task_id='start_task',
+    provide_context=False,
+    python_callable=echo,
+    op_kwargs={'str':'Start the pipeline'},
+    dag=dag,
+)
+
+download_data_from_s3_task = PythonOperator(
+    task_id='download_data_from_s3_task',
+    provide_context=True,
+    python_callable=download_from_s3_task,
+    op_kwargs={'key': 'petrol_consumption.csv', 'bucket_name':'iasd-data-in-the-cloud'},
+    dag=dag,
+)
+
+train_model_task = PythonOperator(
+    task_id='train_model_task',
+    provide_context=True,
+    python_callable=train_model,
+    op_kwargs={'dataset_filepath': data_local_path, 'trained_model_path': trained_model_local_path},
+    dag=dag,
+)
+
+upload_model_to_s3_task = PythonOperator(
+    task_id='upload_to_s3_task',
+    provide_context=True,
+    python_callable=upload_to_s3_task,
+    op_kwargs={'filename':trained_model_local_path, 'key': 'petrol_consumption.csv', 'bucket_name':'iasd-data-in-the-cloud'},
+    dag=dag,
+)
+
+end_task = PythonOperator(
+    task_id='end_task',
+    provide_context=False,
+    python_callable=echo,
+    op_kwargs={'str':'Pipeline ended'},
+    dag=dag,
+)
+
+start_task >> download_data_from_s3_task >> train_model_task >> upload_model_to_s3_task >> end_task
+
